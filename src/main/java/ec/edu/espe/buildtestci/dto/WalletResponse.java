5e863d58c58a3f91c058f4e6eec9f91fd1279432
LuisA-Castillo <lacastillo12@espe.edu.ec>
2026-02-02 15:25:55 -0500

    Proyecto base de gestion de cuenta

---
+++ src/main/java/ec/edu/espe/buildtestci/dto/WalletResponse.java
@@ -0,0 +1,22 @@
+package ec.edu.espe.buildtestci.dto;
+
+import ec.edu.espe.buildtestci.model.Wallet;
+
+public class WalletResponse {
+
+    private final String walletId;
+    private final double balance;
+
+    public WalletResponse(String walletId, double balance) {
+        this.walletId = walletId;
+        this.balance = balance;
+    }
+
+    public String getWalletId() {
+        return walletId;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+}
